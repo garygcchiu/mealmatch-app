7046811ef941b4c622568587058fd3bb2ba38435
Gary <gary.gc.chiu@gmail.com>
2020-11-07 13:43:17 -0500

    standardized eslint and prettier

--- App.tsx
+++ App.tsx
@@ -9,83 +9,80 @@ import awsconfig from './aws-exports';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import {Button, View, Text} from "./components/Themed";
-import { Linking, Platform } from "react-native";
+import { Button, View, Text } from './components/Themed';
+import { Linking, Platform } from 'react-native';
 
 async function urlOpener(url, redirectUrl) {
-  const { type, url: newUrl } = await WebBrowser.openAuthSessionAsync(
-      url,
-      redirectUrl
-  );
+    const { type, url: newUrl } = await WebBrowser.openAuthSessionAsync(
+        url,
+        redirectUrl
+    );
 
-  if (type === 'success' && Platform.OS === 'ios') {
-    WebBrowser.dismissBrowser();
-    return Linking.openURL(newUrl);
-  }
+    if (type === 'success' && Platform.OS === 'ios') {
+        WebBrowser.dismissBrowser();
+        return Linking.openURL(newUrl);
+    }
 }
 
 Amplify.configure({
-  ...awsconfig,
-  oauth: {
-    ...awsconfig.oauth,
-    urlOpener,
-  },
+    ...awsconfig,
+    oauth: {
+        ...awsconfig.oauth,
+        urlOpener,
+    },
 });
 
-
-
 function App(props) {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-  const {
-    oAuthUser,
-    oAuthError,
-    hostedUISignIn,
-    facebookSignIn,
-    googleSignIn,
-    customProviderSignIn,
-    signOut,
-  } = props;
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
+    const {
+        oAuthUser,
+        oAuthError,
+        hostedUISignIn,
+        facebookSignIn,
+        googleSignIn,
+        customProviderSignIn,
+        signOut,
+    } = props;
 
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  function getUser() {
-    return Auth.currentAuthenticatedUser()
-        .then((userData) => userData)
-        .catch(() => console.log('Not signed in'));
-  }
+    function getUser() {
+        return Auth.currentAuthenticatedUser()
+            .then((userData) => userData)
+            .catch(() => console.log('Not signed in'));
+    }
 
-  useEffect(() => {
-    Hub.listen('auth', ({ payload: { event, data } }) => {
-      switch (event) {
-        case 'signIn':
-          getUser().then((userData) => setUser(userData));
-          break;
-        case 'signOut':
-          setUser(null);
-          break;
-        case 'signIn_failure':
-        case 'cognitoHostedUI_failure':
-          console.log('Sign in failure', data);
-          break;
-      }
-    });
+    useEffect(() => {
+        Hub.listen('auth', ({ payload: { event, data } }) => {
+            switch (event) {
+                case 'signIn':
+                    getUser().then((userData) => setUser(userData));
+                    break;
+                case 'signOut':
+                    setUser(null);
+                    break;
+                case 'signIn_failure':
+                case 'cognitoHostedUI_failure':
+                    console.log('Sign in failure', data);
+                    break;
+            }
+        });
 
-    getUser().then((userData) => setUser(userData));
-  }, []);
+        getUser().then((userData) => setUser(userData));
+    }, []);
 
-  if (!isLoadingComplete) {
-    return null;
-  }
+    if (!isLoadingComplete) {
+        return null;
+    }
+    console.log('oAuthUser = ', oAuthUser);
 
-  console.log('oAuthUser = ', oAuthUser);
-
-  return (
-    <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} user={user} />
-        <StatusBar />
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <Navigation colorScheme={colorScheme} user={user} />
+            <StatusBar />
+        </SafeAreaProvider>
+    );
 }
 
 export default withOAuth(App);
